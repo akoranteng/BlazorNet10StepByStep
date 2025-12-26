9d9dd637fdee4307cb0af558c061ad86e0544bb4
akora <akoranteng@msn.com>
2025-12-26 06:28:51 -0500

    Added product service

---
+++ BlazorNet10StepByStep/BlazorNet10StepByStep/Service/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorNet10StepByStep.Models;
+
+namespace BlazorNet10StepByStep.Services
+{
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 999.99m },
+                new Product { Id = 2, Name = "Mouse", Price = 29.99m },
+                new Product { Id = 3, Name = "Keyboard", Price = 49.99m }
+            };
+        }
+    }
+}
