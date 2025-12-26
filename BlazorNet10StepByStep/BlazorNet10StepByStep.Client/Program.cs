2a3fcd9fb5eaa23d41865827e4faac3e3034c742
akora <akoranteng@msn.com>
2025-12-26 12:14:39 -0500

    Registered ProductService in DI container

--- BlazorNet10StepByStep/BlazorNet10StepByStep.Client/Program.cs
+++ BlazorNet10StepByStep/BlazorNet10StepByStep.Client/Program.cs
@@ -2,4 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+//Establish one instance per browser section
+builder.Services.AddScoped<ProductService>();//Provide services to component with DI
+
 await builder.Build().RunAsync();
